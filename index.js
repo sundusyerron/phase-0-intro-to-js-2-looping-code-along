1c883679343faf7a962bee44e08cddb9204fb041
Sundus <sundusaden105@gmail.com>
2022-11-20 23:38:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+
+function writeCards(names,eventName){
+  let array=[]
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+      }
+
+      return array;
+    }
+  }
+
+function countDown(num){
+  while (num >= 0) {
+		console.log(num);
+		num--;
+	}
+}
+
+
+
